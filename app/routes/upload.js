7c3d4b1294a099009c12fc7ec84ce582df1b3229
jundi99 <jundi.robbani99@gmail.com>
2021-03-05 21:37:40 +0700

    add cors

--- app/routes/upload.js
+++ app/routes/upload.js
@@ -1,12 +1,25 @@
 const uploadFina = require('../controllers/upload')
 // const { ReqUserFromToken } = require('../utils')
+const cors = require('cors')
+const corsOptionsDelegate = (req, callback) => {
+  const corsOptions = {
+    origin: 'http://172.104.181.69',
+    optionsSuccessStatus: 200,
+    methods: ['POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 module.exports = (express) =>
   new express.Router()
-    // .use('*', ReqUserFromToken)
-    .get('/', (req, res) => {
-      res.send('respond with a resource')
-    })
-    .post('/item', uploadFina.XLSItem)
-    .post('/price-contract', uploadFina.XLSPriceContract)
+    .options('/price-contract', cors(corsOptionsDelegate))
+    .options('/item', cors(corsOptionsDelegate))
+    .post('/item', cors(corsOptionsDelegate), uploadFina.XLSItem)
+    .post(
+      '/price-contract',
+      cors(corsOptionsDelegate),
+      uploadFina.XLSPriceContract,
+    )
 // .post('/attachment-po', uploadFina.AttachmentPO) not used again because FE use base64 to save pdf/image
